329d8bdca41dc0decc9c6b92a0c4945aab997e63
treeplate <51892395+treeplate@users.noreply.github.com>
2019-11-07 15:24:27 -0800

    Update server.py

--- server.py
+++ server.py
@@ -6,22 +6,27 @@ users = []
 class SimpleChat(WebSocket):
 
     def handleMessage(self):
-       if self.data not in users:
-             users.append(self.data)
+       //if self.data not in users:
+             //users.append(self.data)
        for client in clients:
-             client.sendMessage(users)
-             client.sendMessage(self.address[0] + u' - ' + self.data)
-
+             //client.sendMessage(users)
+             if self.data.startswith("user "):
+                users[clients.index(self)] = self.data[5:];
+                client.sendMessage(self.address[0] + u' is called ' + users[clients.index(self)])
+             else:
+                client.sendMessage(users[clients.index(self)] + ' said "' + self.data + '".')
     def handleConnected(self):
        print self.address, 'connected'
-       self.sendMessage(users);
+       //self.sendMessage(users);
        for client in clients:
           client.sendMessage(self.address[0] + u' - connected')
           
        clients.append(self)
+       users.append("unnamed")    
 
     def handleClose(self):
        clients.remove(self)
+       users.remove(users[clients.index(self)])
        print self.address, 'closed'
        for client in clients:
           client.sendMessage(self.address[0] + u' - disconnected')
