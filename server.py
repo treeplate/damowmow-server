fc38de8293b3cbd520a42dee45eb63099cba758f
treeplate <51892395+treeplate@users.noreply.github.com>
2019-11-24 17:46:10 -0800

    Update server.py

--- server.py
+++ server.py
@@ -6,37 +6,45 @@ users = []
 class SimpleChat(WebSocket):
 
     def handleMessage(self):
-       if self.data.startswith("user "):
-          users[clients.index(self)] = self.data[5:];
-          if(users[clients.index(self)].includes(",")):
-              while users[clients.index(self)].includes(","):
-                users[clients.index(self)][users[clients.index(self)].indexOf(",")] = "-";
-          print self.address[0] + u' is called ' + users[clients.index(self)]
-       elif self.data == "request":
-          self.sendMessage("request:" + (",".join(users)))
-       for client in clients:
-             if self.data.startswith("user "):
-                client.sendMessage("usernamed:"+self.address[0] + u' is called ' + users[clients.index(self)])
-                print self.address[0] + u' is called ' + users[clients.index(self)]
-             elif self.data != "request":
-                print users[clients.index(self)] + "request"
-                client.sendMessage("message:"+users[clients.index(self)] + ' said "' + self.data + '".')
+        try:
+            if self.data.startswith("user "):
+                users[clients.index(self)] = self.data[5:]
+                if(users[clients.index(self)].includes(",")):
+                    while users[clients.index(self)].includes(","):
+                        users[clients.index(
+                            self)][users[clients.index(self)].indexOf(",")] = "-"
+                # print self.address[0] + u' is called ' + users[clients.index(self)]
+            elif self.data == "request":
+                self.sendMessage("request:" + (",".join(users)))
+            for client in clients:
+                if self.data.startswith("user "):
+                    client.sendMessage(
+                        "usernamed:"+self.address[0] + u' is called ' + users[clients.index(self)])
+                    # print self.address[0] + u' is called ' + users[clients.index(self)]
+                elif self.data != "request":
+                    print(users[clients.index(self)] + "request")
+                    client.sendMessage(
+                        "message:"+users[clients.index(self)] + ' said "' + self.data + '".')
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+
     def handleConnected(self):
-       print self.address, 'connected'
-       for client in clients:
-          client.sendMessage("connection:"+self.address[0] + u' - connected')
-          
-       clients.append(self)
-       users.append("unnamed")    
+        print(self.address, 'connected')
+        for client in clients:
+            client.sendMessage("connection:"+self.address[0] + u' - connected')
+
+        clients.append(self)
+        users.append("unnamed")
 
     def handleClose(self):
-       print(users[clients.index(self)] + '- disconnected')
-       for client in clients:
-          client.sendMessage("disconnection:"+users[clients.index(self)] + '- disconnected')
-       users.remove(users[clients.index(self)])
-       clients.remove(self)
-       print self.address, 'closed'
-       
+        print(users[clients.index(self)] + '- disconnected')
+        for client in clients:
+            client.sendMessage(
+                "disconnection:"+users[clients.index(self)] + '- disconnected')
+        users.remove(users[clients.index(self)])
+        clients.remove(self)
+        print(self.address, 'closed')
+
 
 server = SimpleWebSocketServer('', 8001, SimpleChat)
 server.serveforever()
