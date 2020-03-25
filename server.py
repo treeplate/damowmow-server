eed6122de007c117ac8f7665e90b9288c1aa6d5c
treeplate <51892395+treeplate@users.noreply.github.com>
2020-03-25 12:21:34 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,59 +3,40 @@ import traceback
 sys.path.append('/home/treeplate/lib/python')
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 clients = []
-info = []
+
+def satisfy(pattern, input):
+    marks = [true]
+    for x in range(0, pattern.length-1):
+        if pattern[x] == '?':
+            marks.append(input[x])
+        elif pattern[x] == input[x]:
+            
+        else:
+            marks[0] = false
+    return marks
+            
+            
+
 class SimpleChat(WebSocket):
 
     def handleMessage(self):
         try:
-            if self.data.startswith("user "):
-                userinfo = self.data[5:].split(" ")
-                if(userinfo[0] in info):
-                    if(userinfo[1] == info[clients.index(self)][1]):
-                        info[clients.index(self)][2] = True
-                    else:
-                        self.sendMessage("incorrect password")
-                        info[clients.index(self)][2] = False
-                else:
-                    
-                    info[clients.index(self)][0] = userinfo[0]
-                    info[clients.index(self)][1] = userinfo[1]
-                    info[clients.index(self)][2] = True
-                    
-                    print self.address[0] + u' is called ' + info[clients.index(self)][0]
-            elif self.data == "request":
-                users = []
-                for user in info:
-                       users.append(user[0])
-                self.sendMessage("request:" + (",".join(users)))
-                print info[clients.index(self)][0], " has requested."
-            for client in clients:
-                if self.data.startswith("user "):
-                    client.sendMessage("usernamed:"+self.address[0] + u' is called ' + info[clients.index(self)][0])
-                elif self.data != "request":
-                    if(info[clients.index(self)][2] == True):
-                        client.sendMessage("message:"+info[clients.index(self)][0] + ' said "' + self.data + '".')
-                    
+            print(self, ', message')
         except:
-                traceback.print_exc()
+            traceback.print_exc()
 
     def handleConnected(self):
         try:
-            print(self.address, 'connected')
-            for client in clients:
-                client.sendMessage("connection:"+self.address[0] + u' - connected')
             clients.append(self)
-            info.append(["unnamed", "", False])
+            print(self.address, 'connected')
         except:
                 traceback.print_exc()
     def handleClose(self):
-        print(info[clients.index(self)][0] + '- disconnected')
-        for client in clients:
-            client.sendMessage("disconnection:"+info[clients.index(self)][0] + '- disconnected')
-        info[clients.index(self)][2] == False
-        info.remove(info[clients.index(self)]);
-        clients.remove(self)
-        print(self.address, 'closed')
+        try:
+            clients.remove(self)
+            print(self.address, 'closed')
+        except:
+            traceback.print_exc()
 
 
 server = SimpleWebSocketServer('', 8001, SimpleChat)
